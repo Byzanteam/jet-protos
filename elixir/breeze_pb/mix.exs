3141bf1dafa564e39a32e0c299a7ae45c821b854
Phil Chen <06fahchen@gmail.com>
2024-06-25 15:42:37 +0800

    chore: bump to v0.2.0

--- elixir/breeze_pb/mix.exs
+++ elixir/breeze_pb/mix.exs
@@ -4,7 +4,7 @@ defmodule BreezePb.MixProject do
   def project do
     [
       app: :breeze_pb,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
